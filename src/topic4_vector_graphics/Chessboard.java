3fd908ccc67b06bdddea977389eba44412373829
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2022-11-22 23:31:29 +0100

    Add files via upload

--- src/topic4_vector_graphics/Chessboard.java
+++ src/topic4_vector_graphics/Chessboard.java
@@ -34,6 +34,20 @@ public class Chessboard implements Drawing {
 	public void draw(View view) {
 		DrawingUtils.clear(view, Color.gray(0.1));
 		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				
+				// Svetlija i tamnija polja naizmenično
+				if ((i + j) % 2 == 0) {
+					view.setFill(c1);
+				} else {
+					view.setFill(c2);					
+				}
+				
+				// d*(j, i) - d*(m/2, n/2) = d*(j - m/2, i - n/2)
+				view.fillRect(new Vector(d).mul(new Vector(j - m/2.0, i - n/2.0)), new Vector(d));
+			}
+		}
 	}
 	
 	
