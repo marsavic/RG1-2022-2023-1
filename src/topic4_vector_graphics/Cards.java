157072edc6399fd278ca2715dac763ba9ad39ba6
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2022-12-01 21:40:30 +0100

    Add files via upload

--- src/topic4_vector_graphics/Cards.java
+++ src/topic4_vector_graphics/Cards.java
@@ -26,12 +26,33 @@ public class Cards implements Drawing {
 	Vector d = new Vector(16, 16);
 
 	
-	//235023289427398423l - seed
+	void drawCard(View view) {
+		// Okvir karte sa senkom
+		view.setFill(Color.WHITE);
+		view.setEffect(new DropShadow(32, Color.BLACK));
+		view.fillRoundRectCentered(Vector.ZERO, size.div(2), a);
+		view.setEffect(null);
+		view.setStroke(Color.RED);
+		view.setLineWidth(16);
+		view.strokeCircleCentered(Vector.ZERO, r);
+		view.setLineCap(StrokeLineCap.BUTT);
+		view.strokeLine(Vector.polar(r, 1.0 / 8), Vector.polar(r, 5.0 / 8));
+	}
+	
 	
 	@Override
 	public void draw(View view) {
 		DrawingUtils.clear(view, Color.gray(0.125));
-		
+		RNG rng = new RNG(235023289427398423l);
+		view.stateStore();
+		view.setTransformation(Transformation.translation(new Vector(-120, -120)));
+		for(int i = nCards - 1; i >= 0; i--) {
+			view.stateStore();
+			view.addTransformation(new Transformation ().rotate(0.01 * rng.nextGaussian()).translate(d.mul(i)));
+			drawCard(view);
+			view.stateRestore();
+		}
+		view.stateRestore();
 	}
 	
 	
@@ -40,4 +61,3 @@ public class Cards implements Drawing {
 	}
 	
 }
-
